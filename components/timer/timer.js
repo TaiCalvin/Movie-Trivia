06c7a460ce903d098148cb2cc4ec58785cf002c8
Kyle Reed <reedk10@gmail.com>
2018-12-05 20:30:20 -0500

    timer

---
+++ components/timer/timer.js
@@ -0,0 +1,22 @@
+"use strict";
+{
+    function startTimer(){
+        var counter = 60;
+        setInterval(function() {
+          counter--;
+          if (counter >= 0) {
+            span = document.getElementById("count");
+            span.innerHTML = counter;
+          }
+          if (counter === 0) {
+              alert('sorry, out of time');
+              clearInterval(counter);
+          }
+        }, 1000);
+      }
+      $("#startClock").click(function(){
+          startTimer();
+       });
+    
+}
+
