8263c9425a478ead48a2c99c6da84966f11972b8
Megan Eagan <Megan.Eagan@gmail.com>
2018-12-07 21:54:50 -0500

    updated styling

--- components/Difficulty/controller.js
+++ components/Difficulty/controller.js
@@ -23,17 +23,3 @@
         })
 }
 
-// "use strict";
-// {
-//     angular.module('app')
-//         .controller('IntroController', function(TriviaService){
-//             const $ctrl = this;
-
-//             $ctrl.userName = TriviaService.userName;
-
-//             $ctrl.addName = function() {
-//                 window.location = "#!/difficulty"
-//             }
-
-//         })
-// }
